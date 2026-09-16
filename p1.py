08d9c9dcdd48005fd3bd01f4bfb3311c051dbdb7
Shivansh Singla <shivanshsingla12345@gmail.com>
2026-09-16 11:16:13 +0530

    added a python assign

---
+++ p1.py
@@ -0,0 +1,24 @@
+'''
+d={ }
+str = input("enter string:")
+for i in range(len(str)):
+    if str[i] in d:
+        d[str[i]] += 1
+    else:
+        d[str[i]] = 1
+print(d)
+'''
+
+'''
+str = input("enter string:")
+if len(str)<2:
+    print("")
+else:
+    print(str[:2]+str[-2:])
+'''
+
+'''
+a = input("enter string:")
+b = input("enter string:")
+print(b[:2] + a[2:] + " " + a[:2] + b[2:])
+'''
