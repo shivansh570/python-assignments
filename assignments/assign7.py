95b204a2fa2e30347855545bca6dcdcb6ccdd569
Shivansh Singla <shivanshsingla12345@gmail.com>
2026-09-20 01:33:50 +0530

    added assign7

--- assignments/assign7.py
+++ assignments/assign7.py
@@ -1,4 +1,7 @@
 '''
+#1.Write a Python program to count the number of characters (character frequency)
+#in a string.
+
 d={ }
 str = input("enter string:")
 for i in range(len(str)):
@@ -7,9 +10,14 @@ for i in range(len(str)):
     else:
         d[str[i]] = 1
 print(d)
+
 '''
 
 '''
+#2.Write a Python program to get a string made of the first 2 and last 2
+#characters of a given string. If the string length is less than 2, return the
+#empty string instead.
+
 str = input("enter string:")
 if len(str)<2:
     print("")
@@ -18,7 +26,11 @@ else:
 '''
 
 '''
+#3.Write a Python program to get a single string from two given strings, separated
+#by a space and swap the first two characters of each string.
+
 a = input("enter string:")
 b = input("enter string:")
 print(b[:2] + a[2:] + " " + a[:2] + b[2:])
+
 '''
